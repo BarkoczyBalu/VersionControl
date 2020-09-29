fee8016ff58c08041b232e753215dbf953938fe4
Balázs Barkóczy <balazs.barkoczy@stud.uni-corvinus.hu>
2020-09-29 16:05:42 +0200

    Excel import

--- Week04/Form1.cs
+++ Week04/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Week04
 {
     public partial class Form1 : Form
-    {
+    { 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
         public Form1()
